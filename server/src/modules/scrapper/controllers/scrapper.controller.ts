77bd23e3f8d6eebd93ab3154994740c17de6e5a0
Dmytro Levytskyi <dimalevytskyi@gmail.com>
2023-05-20 18:10:02 +0200

    added olx and oglaszamy24 scrappers

--- server/src/modules/scrapper/controllers/scrapper.controller.ts
+++ server/src/modules/scrapper/controllers/scrapper.controller.ts
@@ -14,32 +14,14 @@ export class ScrapperController {
     return await this.offerService.create();
   }
 
-  @Get('/test')
-  getHello(): string {
-    //return this.scrapperService.getHello();
-    return;
-  }
-  @Post('/hello/:id')
-  postHello(
-    @Param('id') id: string,
-    @Query('name') name: string,
-    @Body() json: string,
-  ): string {
-    //return this.scrapperService.getHello()
-    // http://localhost:3000/hello/33221?name=asd
-    console.log(id);
-    console.log(name);
-    console.log(json);
-    return;
-  }
-
   @Get('/olxOffers')
   getOlxOffers() {
-    return this.scrapperService.scrappOlxJobsOfferLinks(10);
+    return this.scrapperService.scrappOlxJobOffers();
   }
 
-  @Get('/scrappOlxJobOffer')
-  getScrappedOlxJobOffer(@Query('url') url: string) {
-    return this.scrapperService.scrappOlxJobOffer(url);
+  @Get('/oglaszamy24plOffers')
+  getOglaszamy24Offers() {
+    return this.scrapperService.scrappOglaszamy24JobOffers();
   }
+
 }
--- server/src/modules/scrapper/services/scrapper.service.ts
+++ server/src/modules/scrapper/services/scrapper.service.ts
@@ -1,27 +1,40 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import * as cheerio from 'cheerio';
-import { lastValueFrom } from 'rxjs';
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
 import * as htmlToText from 'html-to-text';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class ScrapperService {
   constructor(private readonly httpService: HttpService) {}
+  private browser: puppeteer.Browser | null = null;
 
-  public async scrappOlxJobsOfferLinks(pageNumber: number) {
+  private startBrowser = async () => {
+    if (!this.browser) {
+      this.browser = await puppeteer.launch();
+    }
+  };
+
+  private closeBrowser = async () => {
+    if (this.browser) {
+      await this.browser.close();
+      this.browser = null;
+    }
+  };
+
+  private async scrappOlxJobsOfferLinks(pageNumber: number) {
     const url = 'https://www.olx.pl/praca/';
-    const browser = await puppeteer.launch();
+    const browser = this.browser;
     const page = await browser.newPage();
     await page.goto(url);
-    const paginationForward = await page.waitForSelector(
-      '::-p-xpath(//a[@data-cy="pagination-forward"])',
-    );
 
     const offerLinks = [];
     let currentPage = 1;
     while (currentPage <= pageNumber) {
       console.log('Getting offer links from page number:', currentPage);
+      const paginationForward = await page.waitForSelector(
+        '::-p-xpath(//a[@data-cy="pagination-forward"])',
+      );
       try {
         offerLinks.push(
           ...(await page.evaluate(() => {
@@ -40,30 +53,127 @@ export class ScrapperService {
       currentPage++;
       await paginationForward.click();
     }
-    await browser.close();
     return offerLinks;
   }
 
-  public async scrappOlxJobOffer(url: string) {
-    console.log('Scrapping job offer from url:', url);
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+  private async scrappOlxJobOffer(url: string) {
+    try {
+      console.log('Scrapping job offer from url:', url);
+      const browser = this.browser;
+      const page = await browser.newPage();
+      await page.goto(url);
 
-    const offerTitleElement = await page.waitForSelector('::-p-xpath(//h1)');
-    const offerTitleValue = await offerTitleElement.evaluate(
-      (el) => el.textContent,
-    );
+      const offerTitleElement = await page.waitForSelector('::-p-xpath(//h1)');
+      const offerTitleValue = await offerTitleElement.evaluate(
+        (el) => el.textContent,
+      );
 
-    const offerDescriptionElement = await page.waitForSelector(
-      '::-p-xpath(//div/h2/..)',
-    );
-    const offerDescriptionHtml = await offerDescriptionElement.evaluate(
-      (el) => el.innerHTML,
-    );
-    const offerDescriptionText = htmlToText.convert(offerDescriptionHtml);
-    return { offerTitleValue, offerDescriptionText };
+      const offerDescriptionElement = await page.waitForSelector(
+        '::-p-xpath(//div/h2/..)',
+      );
+      const offerDescriptionHtml = await offerDescriptionElement.evaluate(
+        (el) => el.innerHTML,
+      );
+      const offerDescriptionText = htmlToText.convert(offerDescriptionHtml);
+      page.close();
+      return { offerTitleValue, offerDescriptionText, offerUrl: url };
+    } catch (error) {
+      console.error(`Error scrapping job offer from url ${url}:`, error);
+    }
   }
 
-  
+  public async scrappOlxJobOffers() {
+    await this.startBrowser();
+    const offerLinks = await this.scrappOlxJobsOfferLinks(
+      Number(process.env.OLX_SCRAPPING_PAGE_NUMBER),
+    );
+    let jobOffers = [];
+
+    //Scrapping 10 offers at a time
+    for (let i = 0; i < offerLinks.length; i++) {
+      const tenOfferLinks = offerLinks.slice(i, i + 10);
+      i += 10;
+      await Promise.all(
+        tenOfferLinks.map((offerLink) => this.scrappOlxJobOffer(offerLink)),
+      ).then((values) => {
+        jobOffers = [...jobOffers, ...values];
+      });
+    }
+    await this.closeBrowser();
+    return jobOffers;
+  }
+
+  private async scrappOglaszamy24JobsOfferLinks(pageNumber: number) {
+    const BASE_URL = 'https://www.oglaszamy24.pl';
+    const browser = this.browser;
+    const page = await browser.newPage();
+    await page.goto(BASE_URL + '/ogloszenia/praca/oferty-pracy/', {
+      waitUntil: 'domcontentloaded',
+    });
+
+    const offerLinks = [];
+    let currentPage = 1;
+    while (currentPage <= pageNumber) {
+      console.log('Getting offer links from page number:', currentPage);
+      const $ = cheerio.load(await page.content());
+      const paginationForwardUrl = $('.resultpages.next')
+        .children('a')
+        .attr('href')
+        .trim();
+      $('a.o_title').each((index, element) => {
+        offerLinks.push($(element).attr('href').trim());
+      });
+
+      currentPage++;
+      await page.goto(BASE_URL + paginationForwardUrl, {
+        waitUntil: 'domcontentloaded',
+      });
+    }
+    return offerLinks;
+  }
+
+  private async scrappOglaszamy24JobOffer(url: string) {
+    try {
+      const browser = this.browser;
+      const page = await browser.newPage();
+      await page.goto(url);
+      const offerTitleElement = await page.waitForSelector('::-p-xpath(//h1)');
+      const offerTitleValue = await offerTitleElement.evaluate(
+        (el) => el.textContent,
+      );
+      const offerDescriptionElement = await page.waitForSelector('#adv_desc');
+      const offerDescriptionHtml = await offerDescriptionElement.evaluate(
+        (el) => el.innerHTML,
+      );
+      const offerDescriptionText = htmlToText.convert(offerDescriptionHtml);
+      page.close();
+      return { offerTitleValue, offerDescriptionText, offerUrl: url };
+    } catch (error) {
+      console.error(`Error scraping job offers from ${url}:`, error);
+      return {};
+    }
+  }
+
+  public async scrappOglaszamy24JobOffers() {
+    await this.startBrowser();
+    const offerLinks = await this.scrappOglaszamy24JobsOfferLinks(
+      Number(process.env.OGLASZAMY24_SCRAPPING_PAGE_NUMBER),
+    );
+    let jobOffers = [];
+
+    //Scrapping 10 offers at a time
+    for (let i = 0; i < offerLinks.length; i++) {
+      const tenOfferLinks = offerLinks.slice(i, i + 10);
+      i += 10;
+      await Promise.all(
+        tenOfferLinks.map((offerLink) =>
+          this.scrappOglaszamy24JobOffer(offerLink),
+        ),
+      ).then((values) => {
+        jobOffers = [...jobOffers, ...values];
+      });
+    }
+    await this.closeBrowser();
+    return jobOffers;
+  }
 }
