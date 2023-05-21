a5dcad37398b1659901369519d2c6ffb42350959
Marek Mikhaltsevich <marek.michalcewicz@gmail.com>
2023-05-21 04:37:55 +0200

    import and export offers in csv

--- server/src/modules/offer/offer.module.ts
+++ server/src/modules/offer/offer.module.ts
@@ -4,9 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfferProcessingMetrics } from './entities/offer-processing-metrics.entity';
 import { OfferService } from './sevices/offer.service';
 import { OfferController } from './controllers/offer.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Offer, OfferProcessingMetrics])],
+  imports: [
+    TypeOrmModule.forFeature([Offer, OfferProcessingMetrics]),
+    MulterModule.register({
+      dest: './uploads', // Set the destination directory for file uploads
+    }),
+  ],
   controllers: [OfferController],
   providers: [OfferService],
   exports: [OfferService],
